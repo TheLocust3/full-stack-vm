a67f35cf34b1d3094cd95e2e1489120f517e0a61
TheLocust3 <jake.kinsella@gmail.com>
2019-01-15 13:06:48 -0500

    Plan out instruction opcodes and add set instructions to executor

--- docs/INSTRUCTIONS.md
+++ docs/INSTRUCTIONS.md
@@ -2,11 +2,28 @@
 
 All instructions that interact with flags reset the flag register first.
 
+## Deciphering Opcodes
+
+Opcodes are 64bit words but are described for convenience as the lowest order 8bits.
+
+Example (nop):
+0000 0000
+
+Charactes found in opcodes:
+
+- rrr = three bit register code found in REGISTERS.md
+
 ## Register Operations
 
 ### set
 
 Sets value stored in register to next four bytes after this instruction.
+
+Exceptions:
+
+- Cannot set F register
+
+Opcode: 1000 0rrr
 
 ## Arithmetic Operations
 
@@ -18,6 +35,8 @@ Flags:
 
 - Overflow: If number is greater than u64::max_value()
 
+Opcode: 0001 0rrr
+
 ### sub
 
 Subtracts register from register A and stores the result in register A.
@@ -26,19 +45,27 @@ Flags:
 
 - Negative: If number is less than 0
 
+Opcode: 0010 0rrr
+
 ## Bitwise Operations
 
 ### and
 
-Runs an and operation on A and a register and stores the result in A.
+Runs an and operation on register A and a register and stores the result in A.
+
+Opcode: 0000 1rrr
 
 ### or
 
-Runs an or operation on A and a register and stores the result in A.
+Runs an or operation on register A and a register and stores the result in A.
+
+Opcode: 0001 0rrr
 
 ### not
 
 Runs a not operation on a register and stores the result in that register.
+
+Opcode: 0001 1rrr
 
 ### shift-left
 
@@ -48,6 +75,8 @@ Flags:
 
 - Overflow: stores old highest order bit if wrap-around isn't selected
 
+Opcode: 0011 1rrr
+
 ### shift-right
 
 Shifts the register to the right one bit and stores the result in that register. Lowest bit is placed on the new highest order bit if operation is selected, otherwise the bit is lost.
@@ -56,52 +85,76 @@ Flags:
 
 - Overflow: stores old lowest order bit if wrap-around isn't selected
 
+Opcode: 0011 0rrr
+
 ## Control Operations
 
 ### jump
 
 Jump to location stored in the next four bytes.
 
+Opcode: 1111 1111
+
 ### jump0
 
 If value stored in register A is 0 jump to location stored in the next four bytes.
+
+Opcode: 1111 1110
 
 ## Memory Operations
 
 ### read8
 
-Reads an a byte of information at location stored in HL and loads it into register
+Reads an a byte of information at location stored in HL and loads it into register.
+
+Opcode: 1rrr 1001
 
 ### read16
 
-Reads 16bits of information at location stored in HL and loads it into register
+Reads 16bits of information at location stored in HL and loads it into register.
+
+Opcode: 1rrr 1010
 
 ### read32
 
-Reads an 32bits of information at location stored in HL and loads it into register
+Reads an 32bits of information at location stored in HL and loads it into register.
+
+Opcode: 1rrr 1011
 
 ### read64
 
-Reads 64 bits of information at location stored in HL and loads it into register
+Reads 64 bits of information at location stored in HL and loads it into register.
+
+Opcode: 1rrr 1100
 
 ### write8
 
 Writes information stored in register to location stored in HL. Only writes lowest order byte in register.
 
+Opcode: 1rrr 0001
+
 ### write16
 
 Writes information stored in register to location stored in HL. Only writes lowest two order bytes in register.
+
+Opcode: 1rrr 0010
 
 ### write32
 
 Writes information stored in register to location stored in HL. Only writes lowest three order bytes in register.
 
+Opcode: 1rrr 0011
+
 ### write64
 
 Writes information stored in register to location stored in HL.
+
+Opcode: 1rrr 0100
 
 ## Miscellaneous
 
 ### nop
 
 Does nothing.
+
+Opcode: 0
--- src/cpu/executor.rs
+++ src/cpu/executor.rs
@@ -2,32 +2,94 @@ use log::{info, error};
 
 use cpu::cpu::CPU;
 use cpu::register::Register;
+use instructions::register;
 use instructions::arithmetic;
 use instructions::miscellaneous;
 use instructions::instruction_return::RegisterReturn;
 
 pub fn execute(cpu: CPU) -> CPU {
-    info!("PC: {}", cpu.pc.value);
+    let mut pc = Register { value: cpu.pc.value + 1 };
+    info!("PC: {}", pc.value);
 
-    let instruction: u16 = cpu.memory.read_16bit(cpu.pc.value);
-
+    let instruction: u64 = cpu.memory.read_64bit(pc.value);
     info!("Instruction: {}", instruction);
 
+    let mut out_cpu = cpu;
+
     match instruction {
-        0x0001 => {
-            let a: Register = cpu.a;
-            let b: Register = cpu.b;
+        0b10000000 => { // set A
+            let a = out_cpu.a;
+            let value = out_cpu.memory.read_64bit(pc.value + 1);
 
-            let register_return: RegisterReturn = arithmetic::add(a, b);
+            let register_return: RegisterReturn = register::set(a, value);
 
-            let new_cpu = cpu.set_a(register_return.out);
-
-            new_cpu.set_f_from_register_return(register_return)
+            out_cpu = out_cpu.set_a(register_return.out);
+            out_cpu = out_cpu.set_f_from_register_return(register_return);
+            
+            pc = Register { value: pc.value + 1 };
         },
-        0x0000 => miscellaneous::nop(cpu),
+        0b10000001 => { // set B
+            let b = out_cpu.b;
+            let value = out_cpu.memory.read_64bit(pc.value + 1);
+
+            let register_return: RegisterReturn = register::set(b, value);
+
+            out_cpu = out_cpu.set_b(register_return.out);
+            out_cpu = out_cpu.set_f_from_register_return(register_return);
+            
+            pc = Register { value: pc.value + 1 };
+        },
+        0b10000010 => { // set C
+            let c = out_cpu.c;
+            let value = out_cpu.memory.read_64bit(pc.value + 1);
+
+            let register_return: RegisterReturn = register::set(c, value);
+
+            out_cpu = out_cpu.set_c(register_return.out);
+            out_cpu = out_cpu.set_f_from_register_return(register_return);
+            
+            pc = Register { value: pc.value + 1 };
+        },
+        0b10000011 => { // set D
+            let d = out_cpu.d;
+            let value = out_cpu.memory.read_64bit(pc.value + 1);
+
+            let register_return: RegisterReturn = register::set(d, value);
+
+            out_cpu = out_cpu.set_d(register_return.out);
+            out_cpu = out_cpu.set_f_from_register_return(register_return);
+            
+            pc = Register { value: pc.value + 1 };
+        },
+        0b10000100 => { // set E
+            let e = out_cpu.e;
+            let value = out_cpu.memory.read_64bit(pc.value + 1);
+
+            let register_return: RegisterReturn = register::set(e, value);
+
+            out_cpu = out_cpu.set_e(register_return.out);
+            out_cpu = out_cpu.set_f_from_register_return(register_return);
+            
+            pc = Register { value: pc.value + 1 };
+        },
+        0b10000110 => { // set HL
+            let hl = out_cpu.hl;
+            let value = out_cpu.memory.read_64bit(pc.value + 1);
+
+            let register_return: RegisterReturn = register::set(hl, value);
+
+            out_cpu = out_cpu.set_hl(register_return.out);
+            out_cpu = out_cpu.set_f_from_register_return(register_return);
+            
+            pc = Register { value: pc.value + 1 };
+        },
+        0b00000000 => {
+            out_cpu = miscellaneous::nop(out_cpu);
+        },
         _ => {
             error!("Instruction: {} not handled", instruction);
-            miscellaneous::nop(cpu)
         }
     }
+
+    out_cpu.set_pc(pc)
 }
