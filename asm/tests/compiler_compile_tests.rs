bf7207efe571479427523495e9c5242eb1bff8ec
TheLocust3 <jake.kinsella@gmail.com>
2019-02-10 15:00:34 -0500

    Add new compile tests

--- asm/tests/compiler_compile_tests.rs
+++ asm/tests/compiler_compile_tests.rs
@@ -130,6 +130,34 @@ fn test_compile_jump0() {
 }
 
 #[test]
+fn test_compile_set() {
+    let instruction = instruction::Instruction::new("SET", "A", "10");
+
+    let compiled = compile::compile_instruction(instruction);
+
+    assert_eq!(compiled.len(), 9);
+    assert_eq!(compiled[0], 0b00001000);
+    assert_eq!(compiled[1], 0);
+    assert_eq!(compiled[2], 0);
+    assert_eq!(compiled[3], 0);
+    assert_eq!(compiled[4], 0);
+    assert_eq!(compiled[5], 0);
+    assert_eq!(compiled[6], 0);
+    assert_eq!(compiled[7], 0);
+    assert_eq!(compiled[8], 10);
+}
+
+#[test]
+fn test_compile_move_a_b() {
+    let instruction = instruction::Instruction::new("MOVE", "A", "B");
+
+    let compiled = compile::compile_instruction(instruction);
+
+    assert_eq!(compiled.len(), 1);
+    assert_eq!(compiled[0], 0b00000001);
+}
+
+#[test]
 fn test_compile_nop() {
     let instruction = instruction::Instruction::new("NOP", "", "");
 
