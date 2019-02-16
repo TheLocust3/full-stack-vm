42980eff3b57db044fe096406c23ca23fe66cb0c
TheLocust3 <jake.kinsella@gmail.com>
2019-02-16 16:41:14 -0500

    Add basic idea for push and pop command

--- asm/src/converter/miscellaneous.rs
+++ asm/src/converter/miscellaneous.rs
@@ -3,7 +3,16 @@ use instruction::Instruction;
 pub fn convert_push_value(value: String) -> Vec<Instruction> {
     let mut compiled: Vec<Instruction> = Vec::new();
 
-    compiled.push(Instruction::new("", "", ""));
+    // TODO: this is infinitely recursive
+    compiled.push(Instruction::new("MOVE", "HL", "(0)"));
+    compiled.push(Instruction::new("MOVE", "A", value));
+    compiled.push(Instruction::new("WRITE64", "A", ""));
+
+    compiled.push(Instruction::new("MOVE", "A", "4"));
+    compiled.push(Instruction::new("ADD", "HL", ""));
+
+    compiled.push(Instruction::new("MOVE", "HL", "0"));
+    compiled.push(Instruction::new("WRITE64", A, ""));
 
     compiled
 }
@@ -11,7 +20,15 @@ pub fn convert_push_value(value: String) -> Vec<Instruction> {
 pub fn convert_push_reg(reg: String) -> Vec<Instruction> {
     let mut compiled: Vec<Instruction> = Vec::new();
 
-    compiled.push(Instruction::new("", "", ""));
+    // TODO: this is infinitely recursive
+    compiled.push(Instruction::new("MOVE", "HL", "(0)"));
+    compiled.push(Instruction::new("WRITE64", reg, ""));
+
+    compiled.push(Instruction::new("MOVE", "A", "4"));
+    compiled.push(Instruction::new("ADD", "HL", ""));
+    
+    compiled.push(Instruction::new("MOVE", "HL", "0"));
+    compiled.push(Instruction::new("WRITE64", A, ""));
 
     compiled
 }
@@ -19,7 +36,16 @@ pub fn convert_push_reg(reg: String) -> Vec<Instruction> {
 pub fn convert_pop(dest: String) -> Vec<Instruction> {
     let mut compiled: Vec<Instruction> = Vec::new();
 
-    compiled.push(Instruction::new("", "", ""));
+    // TODO: this is infinitely recursive
+    compiled.push(Instruction::new("MOVE", "A", "(0)"))
+    compiled.push(Instruction::new("MOVE", "HL", "4"))
+    compiled.push(Instruction::new("SUB", "HL", ""));
+
+    compiled.push(Instruction::new("MOVE", "HL", "0"))
+    compiled.push(Instruction::new("WRITE64", "A", ""))
+
+    compiled.push(Instruction::new("MOVE", "HL", "A"));
+    compiled.push(Instruction::new("READ64", dest, ""));
 
     compiled
 }
