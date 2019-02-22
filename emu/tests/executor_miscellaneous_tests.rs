2b07f5d936bbf5c3e162d28f00c4f5a9ba3efe92
TheLocust3 <jake.kinsella@gmail.com>
2019-02-21 19:51:14 -0500

    Complete push and pop tests

--- emu/tests/executor_miscellaneous_tests.rs
+++ emu/tests/executor_miscellaneous_tests.rs
@@ -3,18 +3,181 @@ mod tests {
     extern crate emu;
     use self::emu::cpu::cpu::CPU;
     use self::emu::cpu::register::Register;
-    use self::emu::cpu::executor_functions::execute_add;
-    use self::emu::cpu::executor_functions::execute_sub;
+    use self::emu::cpu::executor_functions::execute_push;
+    use self::emu::cpu::executor_functions::execute_pop;
+    use self::emu::memory::TOTAL_MEMORY;
 
     #[test]
-    fn test_add_a() {
+    fn test_push_a() {
         let mut cpu: CPU = CPU::new();
+
         cpu = cpu.set_a(Register { value: 10 });
+        cpu = execute_push(cpu, 0b000);
 
-        cpu = execute_add(cpu, 0b000);
-
-        assert_eq!(cpu.pc.value, 0);
+        assert_eq!(cpu.sp.value, TOTAL_MEMORY - 8);
         assert_eq!(cpu.f.value, 0);
-        assert_eq!(cpu.a.value, 20);
+        assert_eq!(cpu.a.value, 10);
+        assert_eq!(cpu.memory.read_64bit(TOTAL_MEMORY - 8), 10);
+    }
+
+    #[test]
+    fn test_push_b() {
+        let mut cpu: CPU = CPU::new();
+
+        cpu = cpu.set_b(Register { value: 10 });
+        cpu = execute_push(cpu, 0b001);
+
+        assert_eq!(cpu.sp.value, TOTAL_MEMORY - 8);
+        assert_eq!(cpu.f.value, 0);
+        assert_eq!(cpu.b.value, 10);
+        assert_eq!(cpu.memory.read_64bit(TOTAL_MEMORY - 8), 10);
+    }
+
+    #[test]
+    fn test_push_c() {
+        let mut cpu: CPU = CPU::new();
+
+        cpu = cpu.set_c(Register { value: 10 });
+        cpu = execute_push(cpu, 0b010);
+
+        assert_eq!(cpu.sp.value, TOTAL_MEMORY - 8);
+        assert_eq!(cpu.f.value, 0);
+        assert_eq!(cpu.c.value, 10);
+        assert_eq!(cpu.memory.read_64bit(TOTAL_MEMORY - 8), 10);
+    }
+
+    #[test]
+    fn test_push_d() {
+        let mut cpu: CPU = CPU::new();
+
+        cpu = cpu.set_d(Register { value: 10 });
+        cpu = execute_push(cpu, 0b011);
+
+        assert_eq!(cpu.sp.value, TOTAL_MEMORY - 8);
+        assert_eq!(cpu.f.value, 0);
+        assert_eq!(cpu.d.value, 10);
+        assert_eq!(cpu.memory.read_64bit(TOTAL_MEMORY - 8), 10);
+    }
+
+    #[test]
+    fn test_push_e() {
+        let mut cpu: CPU = CPU::new();
+
+        cpu = cpu.set_e(Register { value: 10 });
+        cpu = execute_push(cpu, 0b100);
+
+        assert_eq!(cpu.sp.value, TOTAL_MEMORY - 8);
+        assert_eq!(cpu.f.value, 0);
+        assert_eq!(cpu.e.value, 10);
+        assert_eq!(cpu.memory.read_64bit(TOTAL_MEMORY - 8), 10);
+    }
+
+    #[test]
+    fn test_push_f() {
+        let mut cpu: CPU = CPU::new();
+
+        cpu = cpu.set_f(Register { value: 10 });
+        cpu = execute_push(cpu, 0b101);
+
+        assert_eq!(cpu.sp.value, TOTAL_MEMORY - 8);
+        assert_eq!(cpu.f.value, 0);
+        assert_eq!(cpu.memory.read_64bit(TOTAL_MEMORY - 8), 10);
+    }
+
+    #[test]
+    fn test_push_hl() {
+        let mut cpu: CPU = CPU::new();
+
+        cpu = cpu.set_hl(Register { value: 10 });
+        cpu = execute_push(cpu, 0b110);
+
+        assert_eq!(cpu.sp.value, TOTAL_MEMORY - 8);
+        assert_eq!(cpu.f.value, 0);
+        assert_eq!(cpu.hl.value, 10);
+        assert_eq!(cpu.memory.read_64bit(TOTAL_MEMORY - 8), 10);
+    }
+
+    #[test]
+    fn test_pop_a() {
+        let mut cpu: CPU = CPU::new();
+
+        cpu.memory.write_64bit(TOTAL_MEMORY - 8, 10);
+        cpu = cpu.set_sp(Register { value: TOTAL_MEMORY - 8 });
+        cpu = execute_pop(cpu, 0b000);
+
+        assert_eq!(cpu.sp.value, TOTAL_MEMORY);
+        assert_eq!(cpu.f.value, 0);
+        assert_eq!(cpu.a.value, 10);
+        assert_eq!(cpu.memory.read_64bit(TOTAL_MEMORY - 8), 10);
+    }
+
+    #[test]
+    fn test_pop_b() {
+        let mut cpu: CPU = CPU::new();
+
+        cpu.memory.write_64bit(TOTAL_MEMORY - 8, 10);
+        cpu = cpu.set_sp(Register { value: TOTAL_MEMORY - 8 });
+        cpu = execute_pop(cpu, 0b001);
+
+        assert_eq!(cpu.sp.value, TOTAL_MEMORY);
+        assert_eq!(cpu.f.value, 0);
+        assert_eq!(cpu.b.value, 10);
+        assert_eq!(cpu.memory.read_64bit(TOTAL_MEMORY - 8), 10);
+    }
+
+    #[test]
+    fn test_pop_c() {
+        let mut cpu: CPU = CPU::new();
+
+        cpu.memory.write_64bit(TOTAL_MEMORY - 8, 10);
+        cpu = cpu.set_sp(Register { value: TOTAL_MEMORY - 8 });
+        cpu = execute_pop(cpu, 0b010);
+
+        assert_eq!(cpu.sp.value, TOTAL_MEMORY);
+        assert_eq!(cpu.f.value, 0);
+        assert_eq!(cpu.c.value, 10);
+        assert_eq!(cpu.memory.read_64bit(TOTAL_MEMORY - 8), 10);
+    }
+
+    #[test]
+    fn test_pop_d() {
+        let mut cpu: CPU = CPU::new();
+
+        cpu.memory.write_64bit(TOTAL_MEMORY - 8, 10);
+        cpu = cpu.set_sp(Register { value: TOTAL_MEMORY - 8 });
+        cpu = execute_pop(cpu, 0b011);
+
+        assert_eq!(cpu.sp.value, TOTAL_MEMORY);
+        assert_eq!(cpu.f.value, 0);
+        assert_eq!(cpu.d.value, 10);
+        assert_eq!(cpu.memory.read_64bit(TOTAL_MEMORY - 8), 10);
+    }
+
+    #[test]
+    fn test_pop_e() {
+        let mut cpu: CPU = CPU::new();
+
+        cpu.memory.write_64bit(TOTAL_MEMORY - 8, 10);
+        cpu = cpu.set_sp(Register { value: TOTAL_MEMORY - 8 });
+        cpu = execute_pop(cpu, 0b100);
+
+        assert_eq!(cpu.sp.value, TOTAL_MEMORY);
+        assert_eq!(cpu.f.value, 0);
+        assert_eq!(cpu.e.value, 10);
+        assert_eq!(cpu.memory.read_64bit(TOTAL_MEMORY - 8), 10);
+    }
+
+    #[test]
+    fn test_pop_hl() {
+        let mut cpu: CPU = CPU::new();
+
+        cpu.memory.write_64bit(TOTAL_MEMORY - 8, 10);
+        cpu = cpu.set_sp(Register { value: TOTAL_MEMORY - 8 });
+        cpu = execute_pop(cpu, 0b110);
+
+        assert_eq!(cpu.sp.value, TOTAL_MEMORY);
+        assert_eq!(cpu.f.value, 0);
+        assert_eq!(cpu.hl.value, 10);
+        assert_eq!(cpu.memory.read_64bit(TOTAL_MEMORY - 8), 10);
     }
 }
---
+++ emu/tests/instructions_miscellaneous_tests.rs
@@ -0,0 +1,62 @@
+#[cfg(test)]
+mod tests {
+    extern crate emu;
+    use self::emu::cpu::cpu::CPU;
+    use self::emu::cpu::register::Register;
+    use self::emu::instructions::miscellaneous;
+    use self::emu::memory::TOTAL_MEMORY;
+
+    #[test]
+    fn test_push() {
+        let mut cpu: CPU = CPU::new();
+
+        cpu = cpu.set_a(Register { value: 10 });
+        cpu = miscellaneous::push(cpu.a, cpu);
+
+        assert_eq!(cpu.sp.value, TOTAL_MEMORY - 8);
+        assert_eq!(cpu.f.value, 0);
+        assert_eq!(cpu.a.value, 10);
+        assert_eq!(cpu.memory.read_64bit(TOTAL_MEMORY - 8), 10);
+    }
+
+    #[test]
+    fn test_push_underflow() {
+        let mut cpu: CPU = CPU::new();
+
+        cpu = cpu.set_sp(Register { value: 0 });
+        cpu = cpu.set_a(Register { value: 10 });
+        cpu = miscellaneous::push(cpu.a, cpu);
+
+        assert_eq!(cpu.sp.value, 0);
+        assert_eq!(cpu.f.value, 2);
+        assert_eq!(cpu.a.value, 10);
+        assert_eq!(cpu.memory.read_64bit(0), 0);
+    }
+
+    #[test]
+    fn test_pop() {
+        let mut cpu: CPU = CPU::new();
+
+        cpu.memory.write_64bit(TOTAL_MEMORY - 8, 10);
+        cpu = cpu.set_sp(Register { value: TOTAL_MEMORY - 8 });
+        cpu = miscellaneous::pop(0b000, cpu);
+
+        assert_eq!(cpu.sp.value, TOTAL_MEMORY);
+        assert_eq!(cpu.f.value, 0);
+        assert_eq!(cpu.a.value, 10);
+        assert_eq!(cpu.memory.read_64bit(TOTAL_MEMORY - 8), 10);
+    }
+
+    #[test]
+    fn test_pop_overflow() {
+        let mut cpu: CPU = CPU::new();
+
+        cpu.memory.write_64bit(TOTAL_MEMORY - 8, 10);
+        cpu = miscellaneous::pop(0b000, cpu);
+
+        assert_eq!(cpu.sp.value, TOTAL_MEMORY);
+        assert_eq!(cpu.f.value, 1);
+        assert_eq!(cpu.a.value, 0);
+        assert_eq!(cpu.memory.read_64bit(TOTAL_MEMORY - 8), 10);
+    }
+}
